71a3f3fd91dd39befa0760ba3e733e3bca2d6ba6
aan-andiyanaS <aanandiyana66@gmail.com>
2023-06-14 19:13:55 +0700

    menambahkan lagi 1 db

---
+++ Kuliah/Pertemuan10/Latihan.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar mahasiswa</h3>
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td><img src="image/IMG_9234-PhotoRoom.png" height="60px"></td>
+      <td>24060007</td>
+      <td>Aan Andiyana Sandi</td>
+      <td>224060007.mhs@stmikjabar.ac.id</td>
+      <td>Teknik Informatika</td>
+      <td>
+        <a href="">ubah</a> | <a href="">hapus</a>
+      </td>
+    </tr>
+  </table>
+</body>
+
+</html>
